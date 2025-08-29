ce91c8af61fa9699ff526ebfb2cadc49762a2a8a
Rohan Maindarkar <122428641+rohanmr@users.noreply.github.com>
2025-08-29 23:20:51 +0530

    added day 15 of code

--- day15.py
+++ day15.py
@@ -1,2 +1,16 @@
+def findLongestSubstring(s):
+    last = {}        
+    left = 0
+    max_len = 0
+
+    for right, ch in enumerate(s):
+        if ch in last and last[ch] >= left:
+            
+            left = last[ch] + 1
+        last[ch] = right
+        max_len = max(max_len, right - left + 1)
+
+    return max_len
 
 
+print(findLongestSubstring("abcabcbb"))  
